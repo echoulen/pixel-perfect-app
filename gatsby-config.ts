247a8b9dc8a54210895c8c77c27daf33d74c5b4a
carlos <echoulen@gmail.com>
2022-04-30 09:45:59 +0800

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby"
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `pixel-perfect-app`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
+
+export default config
